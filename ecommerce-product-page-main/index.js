e08bdaa43b77b312ab1bc92d5ff6e74e83004db6
cayto <aidarous1143@gmail.com>
2022-04-25 08:47:44 +0300

    frist project

--- ecommerce-product-page-main/index.js
+++ ecommerce-product-page-main/index.js
@@ -3,32 +3,33 @@ const slides = document.querySelectorAll(".veiw__thumbnail__sub");
 const thumbnail = document.querySelector(".veiw__thumbnail__main");
 const orderdecrease = document.querySelector(".order__btn--1");
 const orderincrease = document.querySelector(".order__btn--2");
-const orderNumber = document.querySelector(".order__number");
+let orderNumber = document.querySelector(".order__number");
 const price = document.querySelector(".price__h2");
 const totalAmount = document.querySelector(".popup__content__info__span");
 const popupCon = document.querySelector(".popup--con");
 const popup = document.querySelector(".popup");
 const cartIcon = document.querySelector(".header__profile__cart");
 // FUNCTIONS change thummails into onther image
-let order = 1;
-orderNumber.textContent = order;
 
+// init function
+var order = 1;
+orderNumber.textContent = order;
+let NumberOrder = 0;
+console.log(orderNumber);
 function wrapper() {
   selecteImage();
   increment();
   decrement();
-  modalOpen();
-  modalclose();
   totalPrice();
 }
 
+// SELECT IMAGE
 const selecteImage = () => {
   thumbnail.src = slides[0].src;
   addEventListener("click", function (e) {
     if (e.target.classList.contains("veiw__thumbnail__sub")) {
       slides.forEach((slide) => {
         const Id = e.target.getAttribute("data-id");
-        console.log(Id);
         if (slide.classList.contains("veiw__thumbnail__sub--active")) {
           slide.classList.remove("veiw__thumbnail__sub--active");
           for (let i = 0; i < slides.length; i++) {
@@ -43,41 +44,42 @@ const selecteImage = () => {
   });
 };
 
-
+// INCREMENT OR DECREMENT ORDER NUMBER
 const increment = () => {
-  orderincrease.addEventListener("click", () => {
-    order++;
-    orderNumber.textContent = order;
-  });
+  order++;
+  orderNumber = order;
 };
 
 const decrement = () => {
-  orderdecrease.addEventListener("click", () => {
-    order--;
-    if (order < 1) {
-      orderNumber.textContent = 1;
-    } else {
-      orderNumber.innerHTML = order;
-    }
-  });
+  order--;
+  if (order < 1) {
+    orderNumber.textContent = 1;
+  } else {
+    orderNumber = order;
+  }
 };
 
+// EVENT LISTENERS
+orderincrease.addEventListener("click", increment);
+orderdecrease.addEventListener("click", decrement);
 
 const totalPrice = () => {
-  console.log(order);
+  console.log(NumberOrder);
 };
 
+//MODAL
+
 const modalOpen = () => {
-  cartIcon.addEventListener("click", () => {
-    popupCon.classList.add("popup--open");
-    popup.classList.add("popup--open");
-  });
+  popup.classList.remove("hidden");
+  popupCon.classList.remove("hidden");
 };
 const modalclose = () => {
-  popupCon.addEventListener("click", () => {
-    popupCon.classList.remove("popup--open");
-    popup.classList.remove("popup--close");
-  });
+  popup.classList.add("hidden");
+  popupCon.classList.add("hidden");
 };
+
+//EVENT LISTENERS
+cartIcon.addEventListener("click", modalOpen);
+popupCon.addEventListener("click", modalclose);
 
 wrapper();
